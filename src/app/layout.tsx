48f7897844ddc22038b7c83ae1e3555165223a55
Divyam19 <divyamgupta19@gmail.com>
2024-12-07 11:16:58 +0530

    init uploadthing
    basic structure is ready but still ui and commands need to be fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider, SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,13 +33,16 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          
-          <SignedOut>
-            <SignIn routing="hash" />
-          </SignedOut>
-          <SignedIn>
-            {children}
-          </SignedIn>
+          <NextSSRPlugin
+            /**
+            * The `extractRouterConfig` will extract **only** the route configs
+            * from the router to prevent additional information from being
+            * leaked to the client. The data passed to the client is the same
+            * as if you were to fetch `/api/uploadthing` directly.
+            */
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
+          {children}
         </body>
       </html>
     </ClerkProvider>
