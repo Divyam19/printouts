ea9bd029868c69a7c5690fc87ea660f2d0c2aa94
Divyam19 <divyamgupta19@gmail.com>
2025-01-05 11:58:20 +0530

    ff

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { UploadButton } from "../utils/uploadthing";
+import { UploadButton , UploadDropzone } from "../utils/uploadthing";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton
+      <UploadDropzone
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
           // Do something with the response
